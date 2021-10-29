00502d2d2e95002f4bfa578fc6ac8e181e46a34a
karanpargal <83785975+karanpargal@users.noreply.github.com>
2021-10-29 23:34:41 +0530

    Add files via upload

---
+++ ytdownload.py
@@ -0,0 +1,23 @@
+from tkinter import *
+from pytube import YouTube
+
+gui= Tk()
+gui.geometry('700x500')
+gui.resizable(0,0)
+gui.title("YT Video Downloader")
+
+link=StringVar()
+
+Label(gui, text='Enter the link here', font='arial 20 bold').place(x=220,y=120)
+link_enter = Entry(gui, width = 30,textvariable = link).place(x = 250, y = 200)
+
+def download():
+    url=YouTube(str(link.get()))
+    url.streams.order_by('resolution')
+    video=url.streams.get_highest_resolution()
+    video.download("C:\\Users\\karan\\Downloads")
+    Label(gui,text="Downloaded", font='arial 20 bold').place(x=260,y=320)
+
+Button(gui,text="Download",font='arial 20 bold',bg='green',padx=2, command=download).place(x=260,y=260)
+
+gui.mainloop()
